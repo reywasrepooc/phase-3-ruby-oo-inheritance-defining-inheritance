1fef05860c737b7dab0ca04ddd7621b663366118
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-08-12 08:53:00 -0400

    commit tree d42243c125bc754f1bfcb3061cf3683e562c6805

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,2 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-require_relative './vehicle'
+require_relative "./vehicle"
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
 
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrooooooooooommmmmm!"
+  end
+
+  def fill_up_tank
+    "Filling up!"
+  end
 end
